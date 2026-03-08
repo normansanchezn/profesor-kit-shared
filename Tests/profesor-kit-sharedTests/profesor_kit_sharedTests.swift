2dadd130e8d581fa8dcbf6576fd53c6ca01e24b1
Norman Sanchez <normansanchezn@gmail.com>
2026-03-08 09:57:26 -0600

    [✨] Testing

--- Tests/profesor-kit-sharedTests/profesor_kit_sharedTests.swift
+++ Tests/profesor-kit-sharedTests/profesor_kit_sharedTests.swift
@@ -6,4 +6,11 @@ final class profesor_kit_sharedTests: XCTestCase {
     func testSharedVersionIsAvailable() {
         XCTAssertEqual(ProfesorKitShared.version, "0.1.0")
     }
+
+    func testSharedVersionHasSemVerShape() {
+        let components = ProfesorKitShared.version.split(separator: ".")
+
+        XCTAssertEqual(components.count, 3)
+        XCTAssertTrue(components.allSatisfy { Int($0) != nil })
+    }
 }
